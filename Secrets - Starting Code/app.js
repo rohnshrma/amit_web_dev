115cc7f941dad3123215e8b854750a6840009575
rohnshrma <rohn11shrma@gmail.com>
2025-07-29 13:51:45 +0530

    feat: implement user authentication with passport and session management

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -1,7 +1,10 @@
 //jshint esversion:6
 
 import express from "express";
-
+import session from "express-session";
+import User from "./models/User.js";
+import passport from "passport";
+import { Strategy as LocalStartegy } from "passport-local";
 import connectDB from "./config/db.js";
 import routes from "./routes/routes.js";
 import dotenv from "dotenv";
@@ -18,6 +21,51 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new LocalStartegy(async (username, password, cb) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return cb(null, false, { message: "incorrect username" });
+      }
+
+      const isMatch = user.comparePassword(password);
+      if (!isMatch) {
+        return cb(null, false, { message: "incorrect password" });
+      }
+
+      return cb(null, user);
+    } catch (err) {
+      return cb(err);
+    }
+  })
+);
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(async (id, cb) => {
+  try {
+    const user = await User.findById(id);
+    cb(null, user);
+  } catch (err) {
+    cb(err);
+  }
+});
+
+// routes
 app.use(routes);
 
 app.listen(PORT, () => {
--- Secrets - Starting Code/models/User.js
+++ Secrets - Starting Code/models/User.js
@@ -10,6 +10,17 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
